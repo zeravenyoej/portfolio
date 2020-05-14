3c31e5fe31f94663a4491d2ffb732cc5a4f1c5ed
Joey Nevarez <josephnevarez12@gmail.com>
2020-05-14 13:00:39 -0700

    adds more skeleton to projects, including project link icons

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-
-
 function App() {
 
   return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import ReactCardFlip from 'react-card-flip';
 import NavBar from './NavBar';
-
+import flipArrow from '../images/flipArrow.png';
+import github from '../images/github.png';
+import internet from '../images/internet.png';
 
 function Projects () {
+  const history = useHistory();
+  const [signFlipped, setSignFlipped] = useState(false);
+  const [personalFlipped, setPersonalFlipped] = useState(false);
+
+
 
   return (
     <>
-     <NavBar/>
+      <NavBar/>
+
+      <div className="projects">
+        <section>
+          <ReactCardFlip isFlipped={signFlipped}>
+            <div onClick={()=>setSignFlipped(!signFlipped)} className="projectCard">
+              <div className="projectCardWithImage">
+                <img src={flipArrow} alt="flip arrow"/>
+              </div>
+            </div>
+
+            <div onClick={()=>setSignFlipped(!signFlipped)} className="projectCard">
+                <img src={flipArrow} alt="flip arrow"/>
+                <p>description of the project</p>
+                <p>dates I worked on it</p>
+                <p>what I did, specifically</p>
+                <p>Tech used</p>
+            </div>
+          </ReactCardFlip>
+
+          <div className="projectsUnderCard">
+            <h1>SignLingo</h1>
+            <div className="projectLinks">
+              <img src={github} alt="link to GitHub repo" onClick={()=>history.push("")}/>
+              <img src={internet} alt="link to deployed site" onClick={()=>history.push("")}/>
+            </div>
+          </div>
+          
+        </section>
+
+        <section>
+          <ReactCardFlip isFlipped={personalFlipped}>
+            <div onClick={()=>setPersonalFlipped(!personalFlipped)} className="projectCard">
+              <div className="projectCardWithImage">
+                <img src={flipArrow} alt="flip arrow"/>
+              </div>
+            </div>
+
+            <div onClick={()=>setPersonalFlipped(!personalFlipped)} className="projectCard">
+                <img src={flipArrow} alt="flip arrow"/>
+                <p>description of the project</p>
+                <p>dates I worked on it</p>
+                <p>what I did, specifically</p>
+                <p>Tech used</p>
+            </div>
+          </ReactCardFlip>
+
+          <div className="projectsUnderCard">
+            <h1>Personal project</h1>
+            <div className="projectLinks">
+              <img src={github} alt="link to GitHub repo" onClick={()=>history.push("")}/>
+              <img src={internet} alt="link to deployed site" onClick={()=>history.push("")}/>
+            </div>
+          </div>
+
+        </section>
+      </div>
     </>
   );
 };
