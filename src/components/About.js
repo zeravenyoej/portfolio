b99160b8922464060a05a52e015fcf734d048f32
Joey Nevarez <josephnevarez12@gmail.com>
2020-08-19 14:40:50 -0700

    improves transitions in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import Hamburger from './Hamburger';
-import { Fade, LightSpeed, Zoom } from 'react-reveal';
+import { Fade, LightSpeed, Slide } from 'react-reveal';
 
 function About () {
     const [showContent, setShowContent] = useState(false);
@@ -32,24 +32,40 @@ function About () {
                     <h1 className="titleh1 aboutTitle">about me</h1>
                 </LightSpeed>
                 ) : (
-                <Fade top>
+                <Slide right>
                     <h1 className="titleh1 aboutTitle">about me</h1>
-                </Fade>
-            )}
-        
+                </Slide>
+                )
+            }
 
             <div className="about">
-                <Fade top when={showContent}>
-                    <div id="aboutImage"></div>
-                </Fade>
+                {intViewportWidth > 1024 ? (
+                    <Fade top when={showContent}>
+                        <div id="aboutImage"></div>
+                    </Fade>
+                ) : (
+                    <Fade top>
+                        <div id="aboutImage"></div>
+                    </Fade>
+                )}
 
-                <Fade bottom when={showContent}>
-                    <div id="aboutText">
-                        <p>Hello! My name is Joseph, but everyone calls me Joey. After 9 years of teaching English as a Second Language (ESL), I decided to pursue a career change in software engineering. Not until entering this field could I truly blend my talents for meticulous detail-orientation and solution-based creativity. Working with international students taught me to think globally; working with software has taught me to think granularly.</p><br/>
-                        <p>These days, you can find me coding in a cafe, where I contribute regularly to robust web applications. I work best when balancing independent and communal work. I value space to think through problems by myself, as well as communities that invite respectful collaboration. And though I am not a trained designer, I always code with user-friendliness in mind. To me, that means intuitive functionality and appealing interfaces.</p><br/>
-                        <p>When I’m not building a site, I’m typically watching, listening to podcasts about, and reading/writing about movies. You can read my short movie reviews <a target="_blank" rel="noopener noreferrer" href="https://letterboxd.com/zeravenyoej">here</a> and more about my experience at Lambda School <a target="_blank" className="medium" rel="noopener noreferrer" href="https://medium.com/@josephnevarez12">here</a>. Looking forward to hearing from you! </p>
-                    </div>
+                {intViewportWidth > 1024 ? (
+                    <Fade bottom when={showContent}>
+                        <div id="aboutText">
+                            <p>Hello! My name is Joseph, but everyone calls me Joey. After 9 years of teaching English as a Second Language (ESL), I decided to pursue a career change in software engineering. Not until entering this field could I truly blend my talents for meticulous detail-orientation and solution-based creativity. Working with international students taught me to think globally; working with software has taught me to think granularly.</p><br/>
+                            <p>These days, you can find me coding in a cafe, where I contribute regularly to robust web applications. I work best when balancing independent and communal work. I value space to think through problems by myself, as well as communities that invite respectful collaboration. And though I am not a trained designer, I always code with user-friendliness in mind. To me, that means intuitive functionality and appealing interfaces.</p><br/>
+                            <p>When I’m not building a site, I’m typically watching, listening to podcasts about, and reading/writing about movies. You can read my short movie reviews <a target="_blank" rel="noopener noreferrer" href="https://letterboxd.com/zeravenyoej">here</a> and more about my experience at Lambda School <a target="_blank" className="medium" rel="noopener noreferrer" href="https://medium.com/@josephnevarez12">here</a>. Looking forward to hearing from you! </p>
+                        </div>
+                    </Fade>
+                    ) : (
+                    <Fade bottom>
+                        <div id="aboutText">
+                            <p>Hello! My name is Joseph, but everyone calls me Joey. After 9 years of teaching English as a Second Language (ESL), I decided to pursue a career change in software engineering. Not until entering this field could I truly blend my talents for meticulous detail-orientation and solution-based creativity. Working with international students taught me to think globally; working with software has taught me to think granularly.</p><br/>
+                            <p>These days, you can find me coding in a cafe, where I contribute regularly to robust web applications. I work best when balancing independent and communal work. I value space to think through problems by myself, as well as communities that invite respectful collaboration. And though I am not a trained designer, I always code with user-friendliness in mind. To me, that means intuitive functionality and appealing interfaces.</p><br/>
+                            <p>When I’m not building a site, I’m typically watching, listening to podcasts about, and reading/writing about movies. You can read my short movie reviews <a target="_blank" rel="noopener noreferrer" href="https://letterboxd.com/zeravenyoej">here</a> and more about my experience at Lambda School <a target="_blank" className="medium" rel="noopener noreferrer" href="https://medium.com/@josephnevarez12">here</a>. Looking forward to hearing from you! </p>
+                        </div>
                 </Fade>
+                )}
             </div>
         </>
     );
