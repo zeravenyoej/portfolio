ec44dbb5c5941bfb9452486bf7ce9f2683ed5732
Joey Nevarez <josephnevarez12@gmail.com>
2020-08-21 12:04:38 -0700

    adds finished draft of SignLingo discription for desktop view

--- src/components/SingLingo.js
+++ src/components/SingLingo.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
-// import internet from '../images/internet.png';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { Slide } from 'react-reveal';
 
 const SingLingo = () => {
@@ -20,25 +17,6 @@ const SingLingo = () => {
                         <div className="projectCardWithImage signLingoBack"></div>
                     </div>
                 </ReactCardFlip>
-{/* 
-                <div className="stuffUnderCard">
-                    <h1>SignLingo</h1>
-                    <div className="projectLinks">
-                        <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://github.com/Lambda-School-Labs/signlingo-fe">
-                            <FontAwesomeIcon className="link" icon={ faGithub }/>
-                        </a>
-                        <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://master.d2965nx2i7rdu0.amplifyapp.com">
-                            <img src={internet} className="link" alt="link to deployed site"/>
-                        </a>
-                    </div>
-                </div> */}
-
                 <div className="description">
                     <div className="header">
                         <h1>SignLingo</h1>
@@ -47,8 +25,19 @@ const SingLingo = () => {
                     <h5>Tech Stack: React, Redux, Sass, Material UI, React Card Flip, Formik</h5>
                         <br/><hr/>
                     <div className="descriptionText">
-                        <p>SignLingo is a mobile, sign language learning platform built to the specifications of a client on a remote, cross-functional team of 24 over 8 weeks.</p> 
-                        <p>I contributed primarily as a front-end engineer, on the app's first team of web developers. Managed flow of the app's development with prior language teaching expertise</p>
+                        <p>SignLingo is a mobile, sign language learning platform built to the specifications of a client on a remote, cross-functional team of 24 over 8 weeks. My team communicated through daily stand ups with our team lead, frequent pair programming, and periodic progress presentations with our UX Designer/stakeholder. We all touched on every area of the app, but I contributed most significantly to the&nbsp; 
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/signlingo-fe/pull/23">landing page</a>,&nbsp;
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/signlingo-fe/pull/8">Redux SignUp/Login logic</a>,&nbsp;
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/signlingo-fe/pull/38">dashboard</a>, and&nbsp;
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/signlingo-fe/pull/45">alphabet lessons</a>.
+                        </p>
+                        <p>I managed the flow of the app with my prior language curriculum expertise. We focused solely on the alphabet, and so I advocated for a present/practice/product model, and encouraged the team that would inherit the project to use a more robust language education framework to accommodate more complex language features. To further ease the handoff of this project, we routinely created user stories on a trello board and completed a project vision document.</p>
+                        <p>Here’s&nbsp;  
+                            <a target="_blank" rel="noopener noreferrer" href="https://master.d2965nx2i7rdu0.amplifyapp.com">the deployed site</a>,&nbsp;
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/signlingo-fe">the GitHub repository</a>,&nbsp;
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=QgZA837xNhA&feature=emb_logo">the video I made</a> when we were voted into finals of a competition for Lambda School’s best collaborative project, and&nbsp; 
+                            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@josephnevarez12">the blog posts I wrote</a> while working on SignLingo.
+                        </p>
                     </div>
                 </div>
             </section>
