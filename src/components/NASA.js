59817c8d395828b815a139155b4ea12530344802
Joey Nevarez <josephnevarez12@gmail.com>
2020-08-28 14:04:48 -0700

    minor changes to nasa project description

--- src/components/NASA.js
+++ src/components/NASA.js
@@ -26,10 +26,10 @@ const NASA = () => {
                     <h5>Tech Stack: ReactJS, Moment, CSS</h5>
                         <br/><hr/>
                     <div className="descriptionText">
-                        <p>NASA Photo of the Day is a responsive application on which you can see the “astronomy picture of the day”, and read the accompanying metadata (title, description, copyright information). I initially built this project in 2 days by myself, as a means of practicing both my Javascript and React skills. More specifically, I was targeting my emerging ability to work with&nbsp;
+                        <p>NASA Photo of the Day is a responsive application on which you can see NASA's “astronomy picture of the day”, and read the accompanying metadata (title, description, copyright information). I initially built this project in 2 days by myself, as a means of practicing both my Javascript and React skills. More specifically, I was targeting my emerging ability to work with&nbsp;
                             <a target="_blank" rel="noopener noreferrer" href="https://api.nasa.gov">an external API</a>.
                         </p>
-                        <p>Ten months later, I returned to the project to update the styling (including experimenting with CSS animation), clean up with the code itself, and, most notably, extend it by adding an additional feature. The application now allows for the user to select another date, which makes an additional API call and retrieves new data, thus allowing the user full access to the API’s entire catalogue of images.</p>
+                        <p>Ten months later, I returned to the project to update the styling (including experimenting with CSS animation), simplify the code itself, and, most notably, extend it by adding an additional feature. The application now allows for the user to select another date, which makes an additional API call and retrieves new data, thus allowing the user full access to the API’s entire catalogue of images.</p>
                         <p>Here's <a target="_blank" rel="noopener noreferrer" href="https://github.com/zeravenyoej/nasa-photo-of-the-day">the GitHub repository</a> and&nbsp; 
                             <a target="_blank" rel="noopener noreferrer" href="https://nasa-photo-of-the-day-sage.vercel.app">the deployed site</a>.
                         </p>
